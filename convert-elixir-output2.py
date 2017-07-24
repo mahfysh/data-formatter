3e53740c3d84479fb8835bc43a8e81bed50f557e
Miłosz Cisowski <milosz@MacBook-Air-Milosz.local>
2017-07-24 21:37:50 +0200

    What?

---
+++ convert-elixir-output2.py
@@ -0,0 +1,55 @@
+# Python
+
+import os
+import sys
+from tkinter import Tk, filedialog
+
+
+def main():
+    print(sys.version)
+    root = Tk()
+    root.withdraw()
+    rel_path = filedialog.askopenfilename()
+    root.destroy()
+
+    while True:
+        target_path = os.path.dirname(rel_path)
+        filename = input('Specify filename: ')
+        target_path = target_path + '/' + filename + '.txt'
+        if target_path == rel_path:
+            print('Target filename cannot be the same as source filename.')
+        else:
+            break
+
+    with open(rel_path) as f:
+
+        with open(target_path, "w+") as t:
+
+            content = f.read()
+            content = content.replace('}, ', '\n')
+            content = content.replace('{', '')
+            content = content.replace('[', '')
+            content = content.replace(']', '')
+            content = content.replace(':', '')
+            content = content.replace(', ', '\t')
+            content = content.replace('"', '')
+            content = content.replace('name=>', '')
+            content = content.replace('username=>', '')
+            content = content.replace('email=>', '')
+            content = content.replace('phone=>', '')
+            # content = content.replace(',', '\t')
+            t.write(content)
+            print('File created at: ' + target_path)
+            t.close()
+            if not t.closed:
+                print('Target file not closed')
+
+        f.close()
+        if not f.closed:
+            print('Source file not closed')
+
+    print('DONE')
+
+
+if __name__ == '__main__':
+    main()
